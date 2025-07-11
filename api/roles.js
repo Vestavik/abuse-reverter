075f9b89856723d59de86ef60bf876368315d65f
Vestavik <vestavik@proton.me>
2025-07-11 16:23:34 +0200

    Update roles.js

--- api/roles.js
+++ api/roles.js
@@ -1,9 +1,13 @@
 const noblox = require('noblox.js');
 
 module.exports = async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  const groupId = parseInt(req.query.groupId);
+  if (!groupId) return res.status(400).json({ error: 'Missing groupId' });
+
   try {
-    const groupId = parseInt(req.query.groupId);
-    if (!groupId) return res.status(400).json({ error: 'Missing groupId' });
     const roles = await noblox.getRoles(groupId);
     res.json(roles);
   } catch (err) {
